9900d8681acc51c3a2cb4dfc69500cf8d1183369
brendayfernandoRenovacion <brendacorrado22@gmail.com>
2026-08-02 12:26:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 
+let numero = 3;
+
+const contador = document.getElementById("contador");
+const resultado = document.getElementById("resultado");
+
+if (contador && resultado) {
+
+  contador.innerHTML = numero;
+
+  let cuenta = setInterval(() => {
+
+    numero--;
+
+    if (numero > 0) {
+      contador.innerHTML = numero;
+    } else {
+      clearInterval(cuenta);
+
+      contador.classList.add("oculto");
+      resultado.classList.remove("oculto");
+    }
+
+  }, 1000);
+
+}
